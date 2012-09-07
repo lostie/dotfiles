5c66a40d3761797fd6c9af2a829938d55e8b4b42
Sergio Marques <sergio.rtmarques@gmail.com>
2012-09-07 09:34:08 +0100

    First commit on dotfiles repo

---
+++ install.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+##
+### from http://errtheblog.com/posts/89-huba-huba
+##
+home = File.expand_path('~')
+
+Dir['etc/*'].each do |file|
+  next if file =~ /install|readme/i
+  target = File.join(home, ".#{File.basename(file)}")
+  puts "Linking #{File.expand_path file} to #{target}"
+  puts `ln -svfh #{File.expand_path file} #{target}`
+end
